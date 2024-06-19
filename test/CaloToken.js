219bc492bb90316715cdab3bb0544db5a0056074
altamisatmaja <altamisfattah16@gmail.com>
2024-06-19 22:30:41 +0700

    fix: import pkg with adopt es6 module

--- test/CaloToken.js
+++ test/CaloToken.js
@@ -1,5 +1,6 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+import { expect } from "chai";
+import pkg from 'hardhat';
+const { ethers } = pkg;
 
 const NAME = "CaloToken";
 const SYMBOL = "CALO";
