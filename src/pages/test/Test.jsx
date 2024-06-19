ddc42f98b0d890d39f5328bd617d637198295d78
altamisatmaja <altamisfattah16@gmail.com>
2024-06-20 02:39:19 +0700

    feat: finish setup

---
+++ src/pages/test/Test.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useState, useEffect } from "react";
+import reactLogo from "../../assets/icons/react.svg";
+import { ethers } from "ethers";
+
+import CaloToken from "../../abis/CaloToken.json";
+import config from "../../config/config.json";
+
+export default function Test() {
+    return (
+        <>
+            <div>
+                <h1>Test</h1>
+            </div>
+        </>
+    )
+}
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Partner from "../pages/Partner";
+import Test from "../pages/test/Test";
 
 const AppRoutes = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/test/home" element={<Test />} />
         <Route path="/about" element={<About />} />
         <Route path="/partner" element={<Partner />} />
       </Routes>
